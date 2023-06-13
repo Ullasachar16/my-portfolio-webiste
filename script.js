41e39251073842fb33354f8d09905075a8e70878
Ullasachar16 <ullasachar16@gmail.com>
2023-06-13 12:23:52 +0530

    commit

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ document.querySelector('.cross').style.display = 'none';
 
 document.querySelector('.hamburger').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
-    
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.sidebar').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    }
 })
